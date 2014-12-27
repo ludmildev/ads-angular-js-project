d9a98c3e7091de294ae4f018df9eb6e25d337ca8
ludmilzdr <ludmilzdr@gmail.com>
2014-12-27 23:54:12 +0200

    some changes

--- js/app.js
+++ js/app.js
@@ -1,15 +1,8 @@
-/**
- * Created by ludmil on 21.12.2014 г..
- */
 
 var app = angular.module('adsApp', ['ngResource', 'ngRoute'])
 	.config(function ($routeProvider) {
 		$routeProvider.when('/', {
-			templateUrl: 'templates/home.html',
-			controller: 'Home'
+			templateUrl: 'templates/home.html'
 		});
-		$routeProvider.when('/ads', {
-			templateUrl: 'templates/allAds.html',
-			controller: 'AllAdsController'
-		});
+        $routeProvider.otherwise({'redirectTo' : '/'});
 	});
